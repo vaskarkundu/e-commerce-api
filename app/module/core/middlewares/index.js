ecc926fde98f1d1efd61465de993383cab3c5dbe
Vaskar Kundu <vaskarkundu18@gmail.com>
2023-03-29 23:37:25 +0600

    feat : added load model

---
+++ app/module/core/middlewares/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  LoadModel: require("./load.model"),
+};
---
+++ app/module/core/middlewares/load.model.js
@@ -0,0 +1,26 @@
+module.exports = function (options, populate) {
+  return function (req, res, next) {
+    var query = { shop: req.user.shop };
+
+    if (options.scope != undefined && options.key != undefined) {
+      query[options.key] = req[options.scope][options.key];
+    } else if (options.key != undefined) {
+      query[options.key] = req.params[options.key];
+    } else {
+      query["_id"] = req.params.id;
+    }
+
+    var model = options.model.findOne(query);
+    if (options.populate != undefined) {
+      for (let field of options.populate) {
+        model.populate({ path: field });
+      }
+    }
+    model.exec((err, doc) => {
+      if (err) return next(err);
+      if (!doc) return next(404);
+      req["model"] = doc;
+      next();
+    });
+  };
+};
